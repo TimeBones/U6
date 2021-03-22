a852d1ab5126037adf93de0deb1112eaac1d9dde
timebones <bendpenner@gmail.com>
2021-03-22 14:36:14 -0500

    customer partial added

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Customer do
-  permit_params :name, :phone, :email, :note
+  permit_params :name, :phone, :email, :note, :image
+
+  form do |f|
+    f.semantic_errors # erros on :base
+    f.inputs          # builds and inpuir field for every attribute
+    # adds an input field and option to upload a file
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions         # adds the 'submit' and 'cancel' buttons
+  end
 end

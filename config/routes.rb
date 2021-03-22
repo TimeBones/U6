1f2faaadee6c9e7fe2c1f7c6981362ce8d3a6780
timebones <bendpenner@gmail.com>
2021-03-22 15:32:10 -0500

    Customer controller and views with partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'customer/show'
-  get 'customer/index'
+  resources :customers, only: %i[index show order]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "customers#index"
 end
